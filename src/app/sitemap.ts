7dd0c60a73d99da86b662c0a99c9c3258a291843
Mateo SIndija <mtsindija05@gmail.com>
2024-04-25 11:13:34 +0200

    Fixed sitemap date

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,16 +3,25 @@ import getExcursions from '@/app/api/database/getExcursions';
 import formatDate from '@/app/utils/formatDate';
 import { PUBLIC_URL } from '@/constants/constants';
 
+const formatDateEuropean = (dateString: string) => {
+  const dateParts = dateString.split('/');
+
+  return new Date(+dateParts[2], Number(dateParts[1]) - 1, +dateParts[0]);
+};
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const excursions = await getExcursions();
   const excursionsEntries: MetadataRoute.Sitemap = ['hr', 'en'].flatMap(
     (local) => {
       return excursions.map((excursion) => ({
         url: `${PUBLIC_URL}/${local}/tours/${excursion.id}`,
-        lastModified: formatDate(
-          excursion.updatedAt.nanoseconds,
-          excursion.updatedAt.seconds,
+        lastModified: formatDateEuropean(
+          formatDate(
+            excursion.updatedAt.nanoseconds,
+            excursion.updatedAt.seconds,
+            false,
+          ),
         ),
+
         changeFrequency: 'yearly',
         priority: 0.5,
       }));
